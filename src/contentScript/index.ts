cd7ce76f6522921ec28f37ee5f1ba0a7609ed49a
Roland Chelwing <roland.chelwing@gmail.com>
2024-08-09 18:48:44 +0200

    Update index.ts

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,14 +1,8 @@
 function removeBlur() {
   const styleElement = document.createElement('style')
   styleElement.textContent = `
-    * {
-      backdrop-filter: none !important;
-    }
-
-    .btm-media-overlays-container,
-    .btm-media-overlays-container * {
-      background: none;
-    }
+    * { backdrop-filter: none !important; }
+    .btm-media-overlays-container, .btm-media-overlays-container * { background: none; }
   `
 
   document.head.appendChild(styleElement)
